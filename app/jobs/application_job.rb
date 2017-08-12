2ff3808bf1ea4b3c3cb388d1cc02fcc6341d4b69
Tom Sherman <the.tomsherman@gmail.com>
2017-08-12 20:12:01 +0100

    Added BuildLibrary job

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,3 @@
 class ApplicationJob < ActiveJob::Base
+  FILE_TYPES = 'mp4'.freeze
 end
---
+++ app/jobs/build_library_job.rb
@@ -0,0 +1,11 @@
+# Builds a library from scatch
+class BuildLibraryJob < ApplicationJob
+  queue_as :default
+
+  def perform(library)
+    search_pattern = File.join library.path, '**', "*.#{FILE_TYPES}"
+    Dir.glob search_pattern do |file|
+      library.media.create name: File.basename(file)
+    end
+  end
+end
